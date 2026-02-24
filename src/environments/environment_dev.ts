64a319772e63f54360610e28b6fc503d87e63659
carlos.arturo.farias@ibm.com <Carlos.Arturo.Farias@ibm.com>
2026-02-24 00:29:54 -0600

    updated internal secret to avoid auth guard

--- src/environments/environment_dev.ts
+++ src/environments/environment_dev.ts
@@ -3,4 +3,8 @@ export const environment = {
     'mongodb+srv://xixzeroxix:34nj6efH@cluster0.kjzuz.mongodb.net/MovNext',
   //MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/MovNext',
 };
+
 export const JWTSECRET = 'nowc823dNNlqd';
+
+export const INTERNAL_APP_SECRET =
+  'y3RB@5gX#Q6mv4eVZ2Lcz8!upG*M7daFqK$P1sRjHT9NnDbGx^Yf%WAoeLiXU0Ct';
